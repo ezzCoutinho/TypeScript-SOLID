2396e14c22e563120e9fc206ea1f5626a14c70f3
Matheus Coutinho <lammerlopes@hotmail.com>
2024-09-17 14:31:26 -0300

    TypeScript SOLID

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+/* 
+Vantagens e desvantagens de S.O.L.I.D
+
+Primeiro cria, depois refatora.
+
+Vantagens:
+- Código modular.
+- Código reutilizável (D.R.Y -> don't repeat yourself).
+- Código testável, baixo acoplamento.
+- Baixo acoplamento e alta coesão.
+- Código expansível.
+- Separations of concerns (Separação de conceitos).
+- Fácil manutenção.
+Desvantagens:
+- Complexidade.
+- Quantidade de código digitada aumentada.
+- Tempo de desenvolvimento aumenta bastante.
+- Tenha cuidados com: YAGNI(You are'nt gonna need it), KISS(Keep it simple, 
+stupid!).
+*/
